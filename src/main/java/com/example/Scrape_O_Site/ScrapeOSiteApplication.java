1af64b044848ca6c5ffa7b624ee96279a49f72d7
Tyler Engel <62031957+Tyl3r-Engel@users.noreply.github.com>
2022-04-04 13:16:31 -0700

    Add files via upload

---
+++ src/main/java/com/example/Scrape_O_Site/ScrapeOSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.Scrape_O_Site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapeOSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapeOSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Scrape_O_Site/ScrapeOSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Scrape_O_Site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapeOSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
